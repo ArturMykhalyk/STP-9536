c94c51dc644106d7d528c2ed887e66d18ebdae1a
Artur_M <64428009+ArturMykhalyk@users.noreply.github.com>
2025-06-20 13:44:18 +0300

    CREATE HEADER

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -4,8 +4,31 @@ const burgerMenuEl = document.querySelector('[data-visible]');
 
 openBtnEl.addEventListener('click', e => {
   burgerMenuEl.dataset.visible = 'open';
+  burgerMenuEl.addEventListener('click', handleCloseMenu);
+  document.body.style.overflow = 'hidden';
+  setTimeout(() => {
+    document.addEventListener('click', handleClickOutside);
+  });
 });
 
-closeBtnEl.addEventListener('click', e => {
+closeBtnEl.addEventListener('click', closeMenu);
+
+function handleCloseMenu(e) {
+  if (e.target.nodeName !== 'A') {
+    return;
+  }
+  closeMenu();
+}
+function handleClickOutside(e) {
+  if (burgerMenuEl.contains(e.target) || openBtnEl.contains(e.target)) {
+    return;
+  }
+  closeMenu();
+}
+
+function closeMenu() {
+  document.body.style.overflow = '';
   burgerMenuEl.dataset.visible = 'close';
-});
+  burgerMenuEl.removeEventListener('click', handleCloseMenu);
+  document.removeEventListener('click', handleClickOutside);
+}
