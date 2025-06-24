888c099e27977092b34e7dde6bfdfa42c9177fb4
Artur_M <64428009+ArturMykhalyk@users.noreply.github.com>
2025-06-24 22:26:43 +0300

    uopdate stone

--- src/js/animation-stone.js
+++ src/js/animation-stone.js
@@ -2,48 +2,25 @@ const wrapper = document.querySelector('.stone-wrapper');
 let step = 0;
 
 function animateStones() {
-  step += 1.5;
+  step += 1;
 
   const positions = [
-    `top ${40 + Math.sin(step / 20) * 10}px left ${
-      50 + Math.cos(step / 25) * 1
-    }%`,
-    `top ${460 + Math.cos(step / 25) * 4}px left calc(50% + ${
-      151 + Math.sin(step / 30) * 30
-    }px)`,
-    `top ${370 + Math.sin(step / 23) * 6}px left calc(50% - ${
-      129 + Math.cos(step / 28) * 4
-    }px)`,
-    `top ${250 + Math.cos(step / 19) * 3}px left calc(50% - ${
-      20 + Math.sin(step / 22) * 3
-    }px)`,
-    `top ${370 + Math.sin(step / 22) * 5}px left calc(50% + ${
-      468 + Math.cos(step / 27) * 4
-    }px)`,
-    `top ${455 + Math.cos(step / 26) * 2}px left calc(50% + ${
-      320 + Math.sin(step / 21) * 3
-    }px)`,
-    `top ${520 + Math.sin(step / 21) * 4}px left calc(50% + ${
-      452 + Math.cos(step / 24) * 5
-    }px)`,
-    `top ${630 + Math.cos(step / 24) * 25}px left calc(50% - ${
-      190 + Math.sin(step / 23) * 2
-    }px)`,
-    `top ${850 + Math.sin(step / 180) * 200}px left calc(50% + ${
+    `top 30px  left calc(50% + ${180 + Math.cos(step / 180) * 300}px)`,
+    `top 460px left calc(50% + 151px)`,
+    ` top 370px left calc(50% - 129px)`,
+    ` top 250px left calc(50% - 20px)`,
+    `top 370px left calc(50% + 468px)`,
+    `top 455px left calc(50% + 320px)`,
+    ` top 520px left calc(50% + 452px)`,
+    `top ${930 + Math.sin(step / 220) * 280}px left calc(50% - 
+      190px)`,
+    `top ${940 + Math.sin(step / 180) * 300}px left calc(50% + ${
       190 + Math.cos(step / 22) * 3
     }px)`,
-    `top ${1135 + Math.cos(step / 20) * 6}px left calc(50% + ${
-      550 + Math.sin(step / 20) * 4
-    }px)`,
-    `top ${1277 + Math.sin(step / 22) * 4}px left calc(50% - ${
-      245 + Math.cos(step / 19) * 4
-    }px)`,
-    `top ${1305 + Math.cos(step / 23) * 5}px left calc(50% - ${
-      125 + Math.sin(step / 25) * 3
-    }px)`,
-    `top ${1295 + Math.sin(step / 19) * 6}px left calc(50% + ${
-      270 + Math.cos(step / 21) * 4
-    }px)`,
+    ` top 1135px left calc(50% + 550px)`,
+    ` top 1277px left calc(50% - 245px)`,
+    `top 1325px  left  calc(50% + ${140 + Math.cos(step / 180) * 320}px)`,
+    `top 1295px left calc(50% + 270px)`,
   ];
 
   wrapper.style.backgroundPosition = positions.join(', ');
